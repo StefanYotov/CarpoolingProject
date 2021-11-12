664618f6b1517e922f1e54f760f87ce9e1d06a54
Kriskooo <kris.danielov95@gmail.com>
2021-11-12 14:25:10 +0200

    Add create/delete in city service add request/response models,constants and dbset

--- CarpoolingProject.Services/Interfaces/ICityService.cs
+++ CarpoolingProject.Services/Interfaces/ICityService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarpoolingProject.Models.RequestModels;
+using CarpoolingProject.Models.ResponseModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,7 @@ namespace CarpoolingProject.Services.Interfaces
 {
     public interface ICityService
     {
+        Task<InfoResponseModel> CreateCityAsync(CreateCityRequestModel requestModel);
+        Task<InfoResponseModel> DeleteCityAsync(DeleteCityRequestModel requestModel);
     }
 }
--- CarpoolingProject/Controllers/Api/CountryController.cs
+++ CarpoolingProject/Controllers/Api/CountryController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CarpoolingProject.Web.Controllers
+namespace CarpoolingProject.Web.Controllers.Api
 {
     [Route("api/[controller]")]
     [ApiController]
